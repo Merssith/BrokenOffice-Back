5e1c1bb3adde7d27077deaf69ed8702bafe7562d
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-15 18:38:15 -0300

    category model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-//const User = require("./User");
 const Incident = require("./Incident");
 const Item = require("./Item");
+const Category = require("./Category");
 
-module.exports = { Incident, Item };
+module.exports = { Incident, Item, Category };
