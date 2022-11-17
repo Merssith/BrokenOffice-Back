147138baa6790b5e0916f5e14f23a9aabf0ec2c5
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-17 12:23:58 -0300

    ADD: user rol seeder / UPDATE: user seeder

--- seeder/index.js
+++ seeder/index.js
@@ -1,5 +1,4 @@
+const createUserRole = require("./userRole.js");
 const createUsers = require("./users.js");
 
-createUsers();
-//When we have more to seed, we will add a promise to execute the next seed. The seeds are going to have an order, that's why it is necessary to finish a previous one to start a new one.
-//  .then(() => createUserRole())
+createUserRole().then(() => createUsers());
--- seeder/users.js
+++ seeder/users.js
@@ -2,20 +2,40 @@ const userService = require("../services/userService");
 
 const users = [
   {
-    name: "User",
-    lastName: "User",
-    email: "user@user.com",
-    password: "1234",
-    telephone: 1234567,
-    geoCords: "[151515151],[515151212]",
-  },
-  {
     name: "Admin",
-    lastName: "Admin",
+    lastName: "Supremo",
     email: "admin@admin.com",
     password: "1234",
     telephone: 7654321,
     geoCords: "[34234324],[234234234]",
+    userRoleId: 1,
+  },
+  {
+    name: "Juana",
+    lastName: "Perez",
+    email: "juana@user.com",
+    password: "1234",
+    telephone: 1234567,
+    geoCords: "[151515151],[515151212]",
+    userRoleId: 2,
+  },
+  {
+    name: "Nicolas",
+    lastName: "Gomez",
+    email: "nico@user.com",
+    password: "1234",
+    telephone: 6743345,
+    geoCords: "[151515151],[515151212]",
+    userRoleId: 3,
+  },
+  {
+    name: "Mabel",
+    lastName: "Lechuga",
+    email: "mabel@user.com",
+    password: "1234",
+    telephone: 75345,
+    geoCords: "[151515151],[515151212]",
+    userRoleId: 3,
   },
 ];
 
