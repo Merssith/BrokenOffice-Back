74e88f1dff19ba08c9ad989063c0f7969f38debb
patricioimbrogno <patricioimbrogno@gmail.com>
2022-11-17 12:46:33 -0300

    routes without office

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const users = require("./users");
 const incidents = require("./incidents")
 const userRole = require('./userRole')
-const office = require('./office')
 const item = require('./item')
 const category = require('./category')
 
 router.use("/users", users);
 router.use("/incidents", incidents);
 router.use("/userRole", userRole);
-router.use("/office", office)
 router.use('/item', item)
 router.use('/category', category)
 
