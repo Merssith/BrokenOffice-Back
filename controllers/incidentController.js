647e345c7b1339283d2e1f35904b5c293681e463
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-22 14:50:35 -0300

    ADD: INCIDENTS handler errors

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -1,26 +1,10 @@
 const incidentService = require("../services/incidentService.js");
 
 exports.getAllIncidents = (req, res) => {
-  // const { id, userRoleId } = req.user;
-  // let role = "";
-  // if (userRoleId === 1) role = "isSuperAdmin";
-  // if (userRoleId === 2) role = "isAdmin";
-  // if (userRoleId === 2) role = "isUser";
-
-  // incidentService
-  //   .getAllIncidents(id, role)
   incidentService
     .getAllIncidents()
     .then((incidents) => res.status(200).send(incidents))
-    .catch((err) => res.status(400).send(err));
-};
-
-exports.getIncident = (req, res) => {
-  const id = req.params.id;
-  incidentService
-    .getIncident(id)
-    .then((incident) => res.status(200).send(incident))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(500).send(err));
 };
 
 exports.createIncident = (req, res) => {
@@ -28,36 +12,23 @@ exports.createIncident = (req, res) => {
   incidentService
     .createIncident(incident)
     .then((newIncident) => res.status(201).send(newIncident))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(500).send(err));
 };
 
 exports.editIncident = (req, res) => {
   const id = req.params.id;
   incidentService
     .editIncident(id, req.body)
-    .then((updatedIncident) => res.send(updatedIncident))
-    .catch((err) => res.status(400).send(err));
+    .then((updatedIncident) => res.status(202).send(updatedIncident))
+    .catch((err) => res.status(500).send(err));
 };
 
 exports.getByUserId = (req, res) => {
   const userId = req.params.userId;
   incidentService
     .getByUserId(userId)
-    .then((incidents) => res.send(incidents))
-    .catch((err) => res.status(400).send(err));
-};
-
-exports.getSearchedIncidents = (req, res) => {
-  const { status, id } = req.query;
-  
-  let filter = null
-  if (status) filter=status
-  if (id) filter=id
- console.log(filter)
-  incidentService
-    .getSearchedIncidents(filter)
-    .then((searchedIncident) => res.send(searchedIncident))
-    .catch((err) => res.status(400).send(err));
+    .then((incidents) => res.status(200).send(incidents))
+    .catch((err) => res.status(500).send(err));
 };
 
 exports.deleteIncident = (req, res) => {
@@ -65,5 +36,16 @@ exports.deleteIncident = (req, res) => {
   incidentService
     .deleteIncident(id)
     .then(() => res.sendStatus(202))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(500).send(err));
+};
+
+exports.getSearchedIncidents = (req, res) => {
+  const { status, id } = req.query;
+  let filter = null;
+  if (status) filter = status;
+  if (id) filter = id;
+  incidentService
+    .getSearchedIncidents(filter)
+    .then((searchedIncident) => res.status(200).send(searchedIncident))
+    .catch((err) => res.status(500).send(err));
 };
