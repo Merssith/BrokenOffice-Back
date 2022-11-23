64700a3cc52ea97e6b717fef14f07fbb4a5119de
tango72 <juanmaariasrodriguez@gmail.com>
2022-11-23 20:46:47 +0100

    addingEnv

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,17 @@
 const cloudinary = require("cloudinary");
+require('dotenv').config()
 
 cloudinary.config({
-  cloud_name: "dsdiadotw",
-  api_key: "296263448996691",
-  api_secret: "IyFGX5zShzjTj3QURvMCtkYJuDs",
+  cloud_name: process.env.CLOUD_NAME_CLOUDINARY,
+  api_key: process.env.API_KEY_CLOUDINARY,
+  api_secret: process.env.API_SECRET_CLOUDINARY,
 });
+
+// cloudinary.config({
+//   cloud_name: "dsdiadotw",
+//   api_key: "296263448996691",
+//   api_secret: "IyFGX5zShzjTj3QURvMCtkYJuDs",
+// });
 
 const cloudimage = cloudinary;
 
