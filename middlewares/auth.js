0ff1d19443647a056f62f11bbace556f5350d1ff
Merssith <mercedes.salcedo1989@gmail.com>
2022-11-23 11:06:03 -0300

    FIX: change avatar doesnt change payload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,12 @@
 const { validateToken } = require("../config/tokens");
+const userService = require("../services/userService");
 
-function validateAuth(req, res, next) {
+async function validateAuth(req, res, next) {
   const token = req.cookies.token;
   if (!token) return res.sendStatus(401);
-
   const { user } = validateToken(token);
   if (!user) return res.sendStatus(401);
-
-  req.user = user;
-
+  req.user = await userService.getMe(user.id);
   next();
 }
 
