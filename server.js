bb8e10999e289fbdc5fbc424f5c253ef25385d72
Merssith <mercedes.salcedo1989@gmail.com>
2022-12-01 18:47:43 -0300

    ADD: datadog middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const db = require("./config/db");
 const routes = require("./routes");
+const { connect_datadog } = require("./middlewares/datadog");
 
 app.use(express.json({ limit: "10mb", extended: true }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -12,6 +13,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(volleyball);
+app.use(connect_datadog);
 app.use("/api", routes);
 
 db.sync({ force: false })
